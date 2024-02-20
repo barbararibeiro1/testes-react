e75d9e3a4cf034a53d09f91eba18ad54dcddebd7
Barbara Ribeiro <barbararibeiro95@outlook.com>
2024-02-20 18:13:52 -0300

    feat: test not pokemon found

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from "@testing-library/react";
+import renderWithRouter from "../renderWithRouter";
+import App from '../App';
+import userEvent from "@testing-library/user-event";
+
+describe ('Teste o componente <FavoritePokemon.tsx />', () => {
+  it('É exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+    const favoriteMessage = screen.getByText(/No favorite Pokémon found/i);
+    expect(favoriteMessage).toBeInTheDocument();
+  });
+
+})
