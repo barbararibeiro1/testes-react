047e870186833ce248117849ea42dbda9033a4fa
Barbara Ribeiro <barbararibeiro95@outlook.com>
2024-02-20 17:42:16 -0300

    fix: lint

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,14 +1,14 @@
-import { screen  } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Teste o componente <About.tsx />', () => {
   it('A página contém informações sobre a Pokédex', () => {
     renderWithRouter(<App />, { route: '/about' });
-    const titleElement = screen.getByRole('heading', { name: /about pokédex/i })
+    const titleElement = screen.getByRole('heading', { name: /about pokédex/i });
     expect(titleElement).toBeInTheDocument();
   });
-  
+
   it('A página contém um headline h2 com o texto About Pokédex', () => {
     renderWithRouter(<App />, { route: '/about' });
     const textAbout = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
@@ -17,11 +17,11 @@ describe('Teste o componente <About.tsx />', () => {
 
   it('A página contém dois parágrafos com texto sobre a Pokédex', () => {
     renderWithRouter(<App />, { route: '/about' });
-    const paragraphFirst = screen.getByText(/This application simulates a Pokédex/i)
-    expect(paragraphFirst).toBeInTheDocument;
+    const paragraphFirst = screen.getByText(/This application simulates a Pokédex/i);
+    expect(paragraphFirst).toBeInTheDocument();
 
-    const paragraphSecond = screen.getByText(/One can filter Pokémon by type/i)
-    expect (paragraphSecond).toBeInTheDocument;
+    const paragraphSecond = screen.getByText(/One can filter Pokémon by type/i);
+    expect(paragraphSecond).toBeInTheDocument();
   });
 
   it('A página contém uma imagem específica de uma pokédex', () => {
