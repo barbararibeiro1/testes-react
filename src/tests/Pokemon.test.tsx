b947e51357256d4365854af625765df3ef7e1edd
Barbara Ribeiro <barbararibeiro95@outlook.com>
2024-02-21 14:18:53 -0300

    feat: sexto requisito, parte 1

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.tsx />', () => {
+  it('É renderizado um card com as informações de determinado Pokémon', () => {
+    renderWithRouter(<App />);
+
+    // O nome correto do Pokémon deve ser mostrado na tela.
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.textContent).toMatch(/Pikachu/i);
+
+    // O tipo correto do Pokémon deve ser mostrado na tela.
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType.textContent).toMatch(/Electric/);
+
+    // O peso médio do Pokémon deve ser exibido
+    const pokemonWeight = screen.getByText(/Average weight: \d+\.\d kg/i);
+    expect(pokemonWeight).toBeInTheDocument();
+
+    // A imagem do Pokémon deve ser exibida
+    const pokemonImg = screen.getByAltText('Pikachu sprite');
+    expect(pokemonImg).toBeInTheDocument;
+    expect(pokemonImg.src).toBe('https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
+
+
+  });
+});
