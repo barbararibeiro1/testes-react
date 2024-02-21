05232ae0851cb2d0794ae49869cef431d5072bc2
Barbara Ribeiro <barbararibeiro95@outlook.com>
2024-02-20 21:37:49 -0300

    fix: lint

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,7 +1,6 @@
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-import { NotFound } from '../pages';
 
 describe('Teste o componente <NotFound.tsx />', () => {
   it('A página contém um heading h2 com o texto Page requested not found', () => {
@@ -9,10 +8,10 @@ describe('Teste o componente <NotFound.tsx />', () => {
     const notFoundMessage = screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
     expect(notFoundMessage).toBeInTheDocument();
   });
-  
+
   it('A página mostra a imagem com um texto alternativo', () => {
     renderWithRouter(<App />, { route: '/notfound' });
     const notFoundImage = screen.getByRole('img') as HTMLImageElement;
-    expect(notFoundImage.src).toBe('http://localhost:3000/404.gif')
-  })
+    expect(notFoundImage.src).toBe('http://localhost:3000/404.gif');
+  });
 });
